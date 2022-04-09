f33fec8536a3cb36dde0f367cf0adc66b5f66518
gurov-e <pistoleta226@yandex.ru>
2022-04-09 18:58:29 +0300

    added editing of recent messages

--- script.js
+++ script.js
@@ -2,36 +2,56 @@ const txtMessage = document.querySelector('.message')
 const btnSpeak = document.querySelector('.speak')
 const form = document.querySelector('.form')
 
+const messages = []
+let messageIdx = -1
+
 function speak(text) {
-    const message = new SpeechSynthesisUtterance()
-    message.lang = 'ru-RU'
-    message.text = text
-    window.speechSynthesis.speak(message)
+  const message = new SpeechSynthesisUtterance()
+  message.lang = 'ru-RU'
+  message.text = text
+  window.speechSynthesis.speak(message)
 }
 
 function clear() {
-    txtMessage.select()
-    txtMessage.value = ''
-    if (window.innerWidth >= 768) {
-        txtMessage.focus()
-        return
-    }
-    txtMessage.blur()
+  txtMessage.select()
+  txtMessage.value = ''
+  if (window.innerWidth >= 375) {
+    txtMessage.focus()
+    return
+  }
+  txtMessage.blur()
 }
 
 btnSpeak.addEventListener('click', () => {
-    speak(txtMessage.value)
-    clear()
+  speak(txtMessage.value)
+  messages.push(txtMessage.value)
+  clear()
 })
 
 txtMessage.addEventListener('keydown', (e) => {
-    if (e.code === 'Enter' && txtMessage.value && !e.shiftKey) {
-        e.preventDefault()
-        speak(txtMessage.value)
-        clear()
-    }
+  if (e.code === 'Enter' && txtMessage.value && !e.shiftKey) {
+    e.preventDefault()
+    speak(txtMessage.value)
+    messages.push(txtMessage.value)
+    clear()
+  }
 })
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+  e.preventDefault()
+})
+
+form.addEventListener('keydown', (e) => {
+  if (e.code === 'ArrowUp') {
+    if (messageIdx >= -1 && messageIdx < messages.length - 1) {
+      messageIdx = messages.length - 1
+    }
+    txtMessage.value = messages[messageIdx]
+  }
+  if (e.code === 'ArrowDown') {
+    if (messageIdx < messages.length && messageIdx >= 1) {
+      messageIdx--
+    }
+    txtMessage.value = messages[messageIdx]
+  }
 })
